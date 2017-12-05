7ffe26956b651144bc84efa5a1c95d78c4cc7dda
jedaos <jedaos38@gmail.com>
2017-12-05 14:05:54 -0500

    First Review

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,6 +2,19 @@ class PostsController < ApplicationController
   def index
   end
 
-  def show 
+  def show
+    @post = Post.find(params[:id])
   end
+
+  def create
+    @post = Post.new(post_params)
+    @post.save
+
+    redirect_to @post
+  end
+
+  private
+    def post_params
+      params.require(:post).permit(:title, :body)
+    end
 end
